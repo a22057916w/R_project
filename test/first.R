3cf1abd41984a6f4fa7394431c9b5935dea46f74
a22057916w <a22057916w@gmail.com>
2018-12-22 16:37:07 +0800

    test

---
+++ test/first.R
@@ -0,0 +1,16 @@
+# Create two 2x3 matrices.
+matrix1 <- matrix(c(3, 9, -1, 4, 2, 6), nrow = 2)
+print(matrix1)
+
+matrix2 <- matrix(c(5, 2, 0, 9, 3, 4), nrow = 2)
+print(matrix2)
+
+# Add the matrices.
+result <- matrix1 + matrix2
+cat("Result of addition","\n")
+print(result)
+
+# Subtract the matrices
+result <- matrix1 - matrix2
+cat("Result of subtraction","\n")
+print(result)
