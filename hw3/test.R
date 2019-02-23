fe9a90595d557f38e7c3c06780a6d28b46beeda1
a22057916w <a22057916w@gmail.com>
2019-02-24 02:09:07 +0800

    trying Rstudio to slove chinese encoding

--- hw3/test.R
+++ hw3/test.R
@@ -1,6 +1,11 @@
-data <- scan("Test2019.txt", what = "character", skip = 1, nline = 1, encoding="UTF-8")
-
+library(readr)
+library(tmcn)
+#options(stringsAsFactors = FALSE)
+#Sys.setlocale(category = "LC_CTYPE", locale = "Chinese (Traditional)")
+data <- scan("Test2019.txt", what = "character", skip = 1, nline = 2, encoding = "big5")
+x <- '123'
+x2 <- iconv('中文', to = 'UTF-8')
+#Sys.getlocale()
+#Sys.setlocale(locale = "Chinese")
 print(data)
 
-x1 <- "幹"
-writeLines(x1, "abc.txt")
