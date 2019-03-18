c58907ec0e6c42b487235560b06e58b527d819ea
a22057916w <a22057916w@gmail.com>
2019-03-18 16:28:57 +0800

    dev web

--- hw3/app.R
+++ hw3/app.R
@@ -1,18 +1,64 @@
 library(shiny)
+library(shinydashboard)
+library(RMySQL)
 library(data.table)
+con = dbConnect(MySQL(), user = "root", password = "", dbname = "willy", host = '140.136.150.100')
 student <- fread("students_big5.csv")
 
-ui <- fluidPage(
-  mainPanel(tableOutput("students"))
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(
+    sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                      label = "Search..."),
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
+  dashboardBody(
+    # Boxes need to be put in a row (or column)
     
+    tabItems(
+      # First tab content
+      tabItem(tabName = "dashboard",
+              fluidRow(
+                tableOutput("students")
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "widgets",
+              h2("Widgets tab content")
+      )
+    )
+  )
 )
 
 server <- function(input, output) {
   
-  output$students <- renderTable({ 
+  observeEvent(input$searchButton, {
+    print(input$searchText)
+    sql <- paste("SELECT * FROM students WHERE student_id='", input$searchText,"';", sep = " ")
+    res <- dbGetQuery(con, sql)
+    print(res)
+    output$students <- renderTable({
+      return(student)
+    })
+  })
+  output$students <- renderTable({
     return(student)
   })
-  
 }
 
-shinyApp(ui, server)
+main <- function() {
+  
+  
+  students <- dbReadTable(con, name = "students")
+  
+  shinyApp(ui, server)
+  #dbDisconnect(con)
+}
+
+if(interactive()) {
+  main()
+}
